a9820c3400a5515d34bdff81518b10900117a11e
Jhzlo <kjh0010703@naver.com>
2024-10-11 23:47:03 +0900

    feat(weather-data): TwoDisplay 인스턴스 생성

--- Observer_pattern/src/main/java/org/weatherObserver/model/WeatherData.java
+++ Observer_pattern/src/main/java/org/weatherObserver/model/WeatherData.java
@@ -1,15 +1,22 @@
 package org.weatherObserver.model;
 
 import org.weatherObserver.view.OneDisplay;
+import org.weatherObserver.view.TwoDisplay;
 
 public class WeatherData {
     private float temperature;
     private float humidity;
     private float pressure;
     OneDisplay oneDisplay = new OneDisplay();
+    TwoDisplay twoDisplay = new TwoDisplay();
 
     public void measurementChange(){
-        oneDisplay.display(getTemperature(), getHumidity());
+        this.temperature = getTemperature();
+        this.humidity = getHumidity();
+        this.pressure = getPressure();
+
+        oneDisplay.display(this.temperature, this.humidity);
+        twoDisplay.display(this.temperature, this.humidity, this.pressure);
     }
     public void setWeatherData(float temperature, float humidity, float pressure){
         this.temperature = temperature;
