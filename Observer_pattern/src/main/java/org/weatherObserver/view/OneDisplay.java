ecf133a5285bed3b9b5a64b2dd938f714d58e993
JHZLO <kjh0010703@naver.com>
2024-10-11 23:43:04 +0900

    feat(two-display): TwoDisplay 객체 생성

--- Observer_pattern/src/main/java/org/weatherObserver/view/OneDisplay.java
+++ Observer_pattern/src/main/java/org/weatherObserver/view/OneDisplay.java
@@ -2,6 +2,6 @@ package org.weatherObserver.view;
 
 public class OneDisplay {
     public void display(float temperature, float humidity){
-        System.out.println("temperature: " + temperature + "humidity: "+humidity);
+        System.out.println("WeatherData : temperature: " + temperature + ", humidity: "+humidity);
     }
 }
---
+++ Observer_pattern/src/main/java/org/weatherObserver/view/TwoDisplay.java
@@ -0,0 +1,7 @@
+package org.weatherObserver.view;
+
+public class TwoDisplay {
+    public void display(float temperature, float humidity, float pressure){
+        System.out.println("WeatherData : temperature: " + temperature + ", humidity: "+humidity + ", pressure: "+pressure );
+    }
+}
