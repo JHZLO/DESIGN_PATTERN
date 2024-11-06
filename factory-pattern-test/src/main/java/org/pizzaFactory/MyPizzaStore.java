471903e54b3d93c43dd16f1eeb2709e53102c352
Jhzlo <kjh0010703@naver.com>
2024-11-06 17:00:48 +0900

    feat: test#5

---
+++ factory-pattern-test/src/main/java/org/pizzaFactory/MyPizzaStore.java
@@ -0,0 +1,8 @@
+package org.pizzaFactory;
+
+import org.pizzaFactory.factory.HeadStorePizzaFactory;
+import org.pizzaFactory.factory.PizzaFactory;
+
+public class MyPizzaStore {
+    PizzaFactory pizzaFactory = new HeadStorePizzaFactory();
+}
