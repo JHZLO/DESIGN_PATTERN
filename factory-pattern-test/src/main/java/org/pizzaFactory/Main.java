f2fae9e9e70118c9dc38db596a8b1f71bcd0324f
Jhzlo <kjh0010703@naver.com>
2024-11-06 17:10:28 +0900

    feat: test#6 -- 1

--- factory-pattern-test/src/main/java/org/pizzaFactory/Main.java
+++ factory-pattern-test/src/main/java/org/pizzaFactory/Main.java
@@ -1,7 +1,15 @@
 package org.pizzaFactory;
 
+import org.pizzaFactory.factory.PizzaFactory;
+import org.pizzaFactory.pizza.Pizza;
+
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Hello Pizza World");
 
+        MyPizzaStore myPizzaStore = new MyPizzaStore();
+        myPizzaStore.orderPizza("cheese");
+        myPizzaStore.orderPizza("clam");
+        myPizzaStore.orderPizza("pepperoni");
     }
 }
--- factory-pattern-test/src/main/java/org/pizzaFactory/MyPizzaStore.java
+++ factory-pattern-test/src/main/java/org/pizzaFactory/MyPizzaStore.java
@@ -2,7 +2,33 @@ package org.pizzaFactory;
 
 import org.pizzaFactory.factory.HeadStorePizzaFactory;
 import org.pizzaFactory.factory.PizzaFactory;
+import org.pizzaFactory.pizza.Pizza;
+import org.pizzaFactory.pizza.cheese.CheesePizza;
+import org.pizzaFactory.pizza.clam.ClamPizza;
 
 public class MyPizzaStore {
     PizzaFactory pizzaFactory = new HeadStorePizzaFactory();
+
+    public Pizza createPizza(String type){
+        Pizza pizza = null;
+        if (type.equalsIgnoreCase("cheese")) {
+            pizza = pizzaFactory.getCheesePizza();
+        }
+        if (type.equalsIgnoreCase("Clam")) {
+            pizza = pizzaFactory.getClamPizza();
+        }
+        if (type.equalsIgnoreCase("Pepperoni")){
+            pizza = pizzaFactory.getPepperoniPizza();
+        }
+        return pizza;
+    }
+
+    public void orderPizza(String type) {
+        Pizza pizza = createPizza(type);
+
+        pizza.prepare();
+        pizza.bake();
+        pizza.cut();
+        pizza.box();
+    }
 }
