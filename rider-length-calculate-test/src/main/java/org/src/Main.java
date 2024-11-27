d8f770f979251d2ceba1edf209833d8d7b3ce9f5
Jhzlo <kjh0010703@naver.com>
2024-11-27 17:15:38 +0900

    feat(cube-factory): 싱글톤 패턴으로 생성

--- rider-length-calculate-test/src/main/java/org/src/Main.java
+++ rider-length-calculate-test/src/main/java/org/src/Main.java
@@ -1,7 +1,14 @@
 package org.src;
+
 public class Main {
     public static void main(String[] args) {
         Box box = new Box();
 
+        BoxObjectAdapter cube = CubeFactory.getInstance(box);
+
+        System.out.println("Cube 1 Height (cm): " + cube.getHeightCm() + "cm");
+        System.out.println("Cube 1 Volume (cm³): " + cube.getVolume() + "cm³");
+
+        
     }
 }
