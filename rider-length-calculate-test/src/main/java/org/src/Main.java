745f64406fd03eab2eda108df1524de0afa8d102
Jhzlo <kjh0010703@naver.com>
2024-11-27 17:18:21 +0900

    feat(hexahedron-factory): 싱글톤 패턴 생성 메서드

--- rider-length-calculate-test/src/main/java/org/src/Main.java
+++ rider-length-calculate-test/src/main/java/org/src/Main.java
@@ -6,9 +6,14 @@ public class Main {
 
         BoxObjectAdapter cube = CubeFactory.getInstance(box);
 
-        System.out.println("Cube 1 Height (cm): " + cube.getHeightCm() + "cm");
-        System.out.println("Cube 1 Volume (cm³): " + cube.getVolume() + "cm³");
+        System.out.println("Cube Height (cm): " + cube.getHeightCm() + "cm");
+        System.out.println("Cube Volume (cm³): " + cube.getVolume() + "cm³");
 
-        
+        BoxClassAdapter hexahedron = HexahedronFactory.getInstance();
+
+        System.out.println("Hexahedron Width (cm): " + hexahedron.getWidthCm() + "cm");
+        System.out.println("Hexahedron Height (cm): " + hexahedron.getHeightCm() + "cm");
+        System.out.println("Hexahedron Length (cm): " + hexahedron.getLengthCm() + "cm");
+        System.out.println("Hexahedron Volume (cm³): " + hexahedron.getVolume() + "cm³");
     }
 }
