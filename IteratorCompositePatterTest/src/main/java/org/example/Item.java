5d63a7de29fd7a79b08fb47d38682c130da4d36d
Jhzlo <kjh0010703@naver.com>
2024-12-05 16:25:16 +0900

    feat(item): Item abstract class 생성

---
+++ IteratorCompositePatterTest/src/main/java/org/example/Item.java
@@ -0,0 +1,35 @@
+package org.example;
+
+public abstract class Item {
+    public void add(Item item) {
+        throw new UnsupportedOperationException();
+    }
+
+    public void remove(Item item) {
+        throw new UnsupportedOperationException();
+    }
+
+    public Item getChild(int i) {
+        throw new UnsupportedOperationException();
+    }
+
+    public String getName() {
+        throw new UnsupportedOperationException();
+    }
+
+    public String getDescription() {
+        throw new UnsupportedOperationException();
+    }
+
+    public double getPrice() {
+        throw new UnsupportedOperationException();
+    }
+
+    public boolean isVegetarian() {
+        throw new UnsupportedOperationException();
+    }
+
+    public void print() {
+        throw new UnsupportedOperationException();
+    }
+}
