7ee60851711b5b8b7258b60cf4021f871b09e3a1
Jhzlo <kjh0010703@naver.com>
2024-12-05 14:58:13 +0900

    feat(menu-test): printMenu() 메서드 생성

--- IteratorCompositePatterTest/src/main/java/org/example/MenuTest.java
+++ IteratorCompositePatterTest/src/main/java/org/example/MenuTest.java
@@ -8,6 +8,7 @@ public class MenuTest {
         System.out.println("USING FOR LOOPS");
         branchMenu(pancakeHouseMenu);
         dinerMenu(dinerMenu);
+        printMenu(pancakeHouseMenu, dinerMenu); // 통합 메뉴 출력하기
     }
 
     private static void branchMenu(PancakeHouseMenu pancakeHouseMenu){
@@ -20,6 +21,21 @@ public class MenuTest {
     }
 
     private static void dinerMenu(DinerMenu dinerMenu){
+        for (int i = 0; i < dinerMenu.getMenuItems().length; i++) {
+            MenuItem menuItem = dinerMenu.getMenuItems()[i];
+            System.out.print(menuItem.getName());
+            System.out.println("\t\t" + menuItem.getPrice());
+            System.out.println("\t" + menuItem.getDescription());
+        }
+    }
+
+    private static void printMenu(PancakeHouseMenu pancakeHouseMenu, DinerMenu dinerMenu){
+        for (int i = 0; i < pancakeHouseMenu.getMenuItems().size(); i++) {
+            MenuItem menuItem = pancakeHouseMenu.getMenuItems().get(i);
+            System.out.print(menuItem.getName());
+            System.out.println("\t\t" + menuItem.getPrice());
+            System.out.println("\t" + menuItem.getDescription());
+        }
         for (int i = 0; i < dinerMenu.getMenuItems().length; i++) {
             MenuItem menuItem = dinerMenu.getMenuItems()[i];
             System.out.print(menuItem.getName());
