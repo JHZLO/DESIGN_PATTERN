e09742faf379b512c25075b11373266db492fd5a
Jhzlo <kjh0010703@naver.com>
2024-12-07 20:54:39 +0900

    feat: 데코레이터 패턴 프로젝트 생성

---
+++ Decorator-Pattern/star-buds-coffee/src/main/java/org/example/Beverage.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public abstract class Beverage {
+    private String description = null;
+
+    public abstract String getDescription();
+    public abstract double cost();
+}
