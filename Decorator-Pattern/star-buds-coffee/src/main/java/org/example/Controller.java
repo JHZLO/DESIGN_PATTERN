5b4268ef4574afe8631568b30097ab44ae8d7ad2
Jhzlo <kjh0010703@naver.com>
2024-12-07 21:10:42 +0900

    feat: Controller, decorator flow 구현

--- Decorator-Pattern/star-buds-coffee/src/main/java/org/example/Controller.java
+++ Decorator-Pattern/star-buds-coffee/src/main/java/org/example/Controller.java
@@ -1,7 +1,16 @@
 package org.example;
 
+import org.example.coffee.DarkRoast;
+import org.example.coffee.Espresso;
+import org.example.condiment.Milk;
+import org.example.condiment.Mocha;
+import org.example.condiment.Whip;
+
 public class Controller {
     public void run(){
+        Beverage beverage1 = new Milk(new Mocha(new Whip(new DarkRoast())));
 
+        System.out.println("Description: " + beverage1.getDescription());
+        System.out.println("Cost: " + beverage1.cost());
     }
 }
