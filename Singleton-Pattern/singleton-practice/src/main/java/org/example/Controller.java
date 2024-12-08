dd0b9fd9e9ef6d182ac3aa7bc42625d3b7fef402
Jhzlo <kjh0010703@naver.com>
2024-12-08 22:11:53 +0900

    test: 멀티쓰레딩 환경 구축 및 테스트

--- Singleton-Pattern/singleton-practice/src/main/java/org/example/Controller.java
+++ Singleton-Pattern/singleton-practice/src/main/java/org/example/Controller.java
@@ -1,14 +1,23 @@
 package org.example;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Controller {
     public void run() {
-        ChocolateBoiler chocolateBoiler = ChocolateBoiler.getInstance();
-        ChocolateBoiler chocolateBoiler1 = ChocolateBoiler.getInstance();
+        // 스레드 풀 생성 (10개의 스레드가 동시에 실행)
+        ExecutorService executor = Executors.newFixedThreadPool(10);
 
-        chocolateBoiler.fill();
-        chocolateBoiler1.fill();
+        for (int i = 0; i < 10; i++) {
+            executor.submit(() -> {
+                ChocolateBoiler instance = ChocolateBoiler.getInstance();
+                instance.fill();
+                instance.boil();
+                instance.drain();
+                System.out.println("Instance: " + instance);
+            });
+        }
 
-        chocolateBoiler.boil();
-        chocolateBoiler1.boil();
+        executor.shutdown();
     }
 }
